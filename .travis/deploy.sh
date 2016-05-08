aab1cd89c225c898e63cf3ea152dc9a362bd2717
Juri Leino <github@line-o.de>
2016-05-08 16:45:20 +0200

    fix (release): do not check for known host in CDN deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,10 +6,10 @@ chmod 600 .travis/deploy_key.pem # this key should have push access
 ssh-add .travis/deploy_key.pem
 
 echo "create directory $VERSION"
-ssh -p10022 podlove@cdn.podlove.org "mkdir ~/podlove-web-player/$VERSION"
+ssh -p10022 -o StrictHostKeyChecking=no podlove@cdn.podlove.org "mkdir ~/podlove-web-player/$VERSION"
 
 echo 'copy files from travis to CDN'
 cd dist
-scp -P10022 -r . podlove@cdn.podlove.org:podlove-web-player/$VERSION
+scp -P10022 -r -o StrictHostKeyChecking=no . podlove@cdn.podlove.org:podlove-web-player/$VERSION
 
 echo 'Done'
